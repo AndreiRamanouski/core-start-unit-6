9cd6b9351d20677e82e31455d1bfcdad2ebe7075
Andrei Ramanouski <68558820+AndreiRamanouski@users.noreply.github.com>
2021-12-22 14:27:18 +0300

    added a solution to the task 33

---
+++ src/by/htp/ramanouski/task33/Floor.java
@@ -0,0 +1,24 @@
+package by.htp.ramanouski.task33;
+
+public class Floor {
+    private double width;
+    private double height;
+
+    public Floor(double width, double height) {
+        if (width < 0)
+            this.width = 0;
+        else
+            this.width = width;
+
+        if (height < 0)
+            this.height = 0;
+        else
+            this.height = height;
+
+    }
+
+    public double getArea() {
+        return height * width;
+    }
+
+}
